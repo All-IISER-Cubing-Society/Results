17ee80e91b5a50cc2c8d718407150087942b69f2
Purva <thereconpilot@gmail.com>
2022-03-10 14:16:56 +0530

    Best Results Category added

--- Results.py
+++ Results.py
@@ -59,42 +59,70 @@ cdf = load_data()
 
 st.sidebar.title("AICS - Results")
 
-# Get list of names in sorted order
-names = sorted(cdf['Name'].unique())
+category = st.sidebar.radio(
+    'Select Category',
+    ('Individual Results', 'Best Results'))
 
-# Sidebar name selector
-name = st.sidebar.selectbox('Name', names)
+
+if category == 'Individual Results':
+
+    # Get list of names in sorted order
+    names = sorted(cdf['Name'].unique())
+
+    # Sidebar name selector
+    name = st.sidebar.selectbox('Name', names)
+
+    # Person specific data
+    df = cdf[cdf['Name'] == name]
+    institute = df['Institute'].iloc[0]
+
+    st.header(name)
+    st.subheader(institute)
+
+    # Get events
+    events = df['Event'].unique()
+
+    selected_events = st.multiselect('Events', events, '3x3')
+
+    if len(selected_events) > 0:
+        selected_events_df = load_event_data(df, name, selected_events)
+        st.write("The graph is interactive. Feel free to play around with it.")
+        if 'FMC' in selected_events and len(selected_events) > 1:
+            st.write("FMC results are in Moves, and others in seconds. It would be better to plot FMC as a single graph.")
+        fig = px.line(selected_events_df, x='Date', y='Result', color='Event', markers=True)
+
+        st.plotly_chart(fig, use_container_width=True)
+    else:
+        st.write("Please select some events.")
+
+    st.write("If on mobile, select name from sidebar on top left.")
+
+    st.header("Event Participation")
+    participation_df = df['Event'].value_counts().reset_index()
+    participation_df.columns = ['Event', 'Count']
+    participation_df = participation_df.sort_values('Count', ascending=False)
+    st.dataframe(participation_df)
+
+elif category == 'Best Results':
+    events = sorted(cdf['Event'].unique())
+    
+    event = st.sidebar.selectbox('Event', events)
+    
+    df = cdf[cdf['Event'] == event]
+    
+    # First sort by Result, then do a stable sort on Name
+    df = df.sort_values('Result').sort_values('Name', kind='stable')
+    
+    # Drop duplicates, then sort by result again
+    df = df.loc[df['Name'] != df['Name'].shift()].sort_values('Result')
+    
+    df['Result'] = df['Result'].astype(str)
+    df['Date'] = df['Date'].astype(str)
+    df = df.reset_index().drop(columns=['index', 'Event'])
+    
+    st.header(event)
+    st.write(df)
 
 image = Image.open("media/AICS-Logo-Dark.png")
 st.sidebar.image(image)
 st.sidebar.markdown("[Website](https://all-iiser-cubing-society.github.io/#/) | [Instagram](https://www.instagram.com/all.iiser.cubing.society/) | [YouTube](https://www.youtube.com/channel/UCXOIh4FS48Dwy3BC9_FhprA)")
-# Person specific data
-df = cdf[cdf['Name'] == name]
-institute = df['Institute'].iloc[0]
-
-st.header(name)
-st.subheader(institute)
-
-# Get events
-events = df['Event'].unique()
-
-selected_events = st.multiselect('Events', events, '3x3')
-
-if len(selected_events) > 0:
-    selected_events_df = load_event_data(df, name, selected_events)
-    st.write("The graph is interactive. Feel free to play around with it.")
-    if 'FMC' in selected_events and len(selected_events) > 1:
-        st.write("FMC results are in Moves, and others in seconds. It would be better to plot FMC as a single graph.")
-    fig = px.line(selected_events_df, x='Date', y='Result', color='Event', markers=True)
-
-    st.plotly_chart(fig, use_container_width=True)
-else:
-    st.write("Please select some events.")
-
-st.write("If on mobile, select name from sidebar on top left.")
-
-st.header("Event Participation")
-participation_df = df['Event'].value_counts().reset_index()
-participation_df.columns = ['Event', 'Count']
-participation_df = participation_df.sort_values('Count', ascending=False)
-st.dataframe(participation_df)
